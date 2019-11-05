de8601ed42c7b499ead499b459d3e02c2a251419
André Morpanini <andremorpanini@MacBook-Pro-de-Andre.local>
2019-11-05 12:21:18 +0000

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+const App = () => {
+    useEffect(() => {
+        //Init Matrialize JS
+        M.AutoInit();
+    });
+    return (
+        <Fragment>
+            <SearchBar />
+            <div className="container">
+                <Logs />
+            </div>
+        </Fragment>
+    );
+};
 
 export default App;
