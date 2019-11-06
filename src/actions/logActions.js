e647bd6e26625469a9a79f9e494a2a859a5e0cba
André Morpanini <andremorpanini@MacBook-Pro-de-Andre.local>
2019-11-06 09:48:21 +0000

    Search Done

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
     DELETE_LOG,
     SET_CURRENT,
     CLEAR_CURRENT,
-    UPDATE_LOG
+    UPDATE_LOG,
+    SEARCH_LOGS
 } from "./types";
 
 //Get log from server
@@ -89,6 +90,26 @@ export const updateLog = log => async dispatch => {
 
         dispatch({
             type: UPDATE_LOG,
+            payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+};
+
+//Search log from server
+export const searchLogs = text => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs?q=${text}`);
+        const data = await res.json();
+
+        dispatch({
+            type: SEARCH_LOGS,
             payload: data
         });
     } catch (err) {
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -6,7 +6,8 @@ import {
     DELETE_LOG,
     SET_CURRENT,
     CLEAR_CURRENT,
-    UPDATE_LOG
+    UPDATE_LOG,
+    SEARCH_LOGS
 } from "../actions/types";
 
 const initialState = {
@@ -52,6 +53,11 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 current: null
+            };
+        case SEARCH_LOGS:
+            return {
+                ...state,
+                logs: action.payload
             };
         case SET_LOADING:
             return {
